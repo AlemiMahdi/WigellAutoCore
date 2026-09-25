8ad486a2f902b4da2d5e0e1b73dd31f5d5afcdd3
Wall1EMone <mone.larsson@outlook.com>
2026-09-26 00:46:56 +0200

    WAC-27: Sparar bokningar i databasen

---
+++ src/com/wac/autocore/entity/BookingEntity.java
@@ -0,0 +1,53 @@
+package com.wac.autocore.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "bookings")
+public class BookingEntity {
+
+    @Id
+    private int id;
+
+    @Column (name = "vehicle_id")
+    private int vehicleId;
+
+    @Column(name = "booking_date")
+    private LocalDate date;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "status")
+    private String status;
+
+    public BookingEntity() {}
+
+    public int getId(){ return id; }
+
+    public void setId(int id){ this.id = id; }
+
+    public int getVehicleId(){ return vehicleId; }
+
+    public void setVehicleId(int vehicleId){ this.vehicleId = vehicleId; }
+
+    public LocalDate getDate(){ return date; }
+
+    public void setDate(LocalDate date) { this.date = date; }
+
+    public String getDescription(){ return description; }
+
+    public void setDescription(String description){ this.description = description; }
+
+    public String getStatus(){ return status; }
+
+    public void setStatus (String status){ this.status = status;}
+
+
+
+}
